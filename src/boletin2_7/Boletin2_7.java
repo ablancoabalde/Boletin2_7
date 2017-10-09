8d9beca8678857f7e32813aa7b686eeadd9033e2
ablancoabalde <ablancoabalde@victoria6.danielcastelao.org>
2017-10-09 08:53:30 +0200

    Documento ejecutable

---
+++ src/boletin2_7/Boletin2_7.java
@@ -0,0 +1,20 @@
+
+package boletin2_7;
+
+import java.util.Scanner;
+
+public class Boletin2_7 {
+
+    public static void main(String[] args) {
+    
+        float centigrado;
+        Scanner entradagrados = new Scanner (System.in);
+        
+        System.out.println("Introduzca los grados centigrados para pasarlos a grados Kelvin y grados Farenheit");
+        
+        centigrado=entradagrados.nextFloat();
+        
+        System.out.println("Son tantos grados Kelvin " + (centigrado * 283) + " son tantos grados Farenheit " + (1.8*centigrado +32));
+    }
+    
+}
